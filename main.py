68071a3c7956de2941c8a59d589d3508b59aaff2
schwalbe10 <schwalbe10@gmail.com>
2024-06-09 11:30:51 +0900

    0.43181726705574414

--- main.py
+++ main.py
@@ -140,7 +140,8 @@ class VQADataset(torch.utils.data.Dataset):
         image = Image.open(f"{self.image_dir}/{self.df['image'][idx]}")
         image = self.transform(image)
         question = np.zeros(len(self.idx2question) + 1)  # 未知語用の要素を追加
-        question_words = self.df["question"][idx].split(" ")
+        # question_words = self.df["question"][idx].split(" ")
+        question_words = process_text(self.df["question"][idx]).split(" ")  # ここで前処理を行う
         for word in question_words:
             try:
                 question[self.question2idx[word]] = 1  # one-hot表現に変換
